51e59b6de7b02b2fdec0bb231d18f15cdde1c881
Anna Bartha <bartha.anna999@gmail.com>
2020-11-21 12:31:49 +0100

    all

--- week09/UnitTestExample/Controllers/AccountController.cs
+++ week09/UnitTestExample/Controllers/AccountController.cs
@@ -25,13 +25,16 @@ namespace UnitTestExample.Controllers
             if(!ValidateEmail(email))
                 throw new ValidationException(
                     "A megadott e-mail cím nem megfelelő!");
-            if(!ValidateEmail(email))
+            if(!ValidatePassword(password))
                 throw new ValidationException(
                     "A megadottt jelszó nem megfelelő!\n" +
-                    "A jelszó legalább 8 karakter hosszú kell legyen, csak az angol ABC betűiből és számokból állhat, és tartalmaznia kell legalább egy kisbetűt, egy nagybetűt és egy számot.");
+                    "A jelszó legalább 8 karakter hosszú kell legyen, " +
+                    "csak az angol ABC betűiből és számokból állhat, és tartalmaznia kell " +
+                    "legalább egy kisbetűt, egy nagybetűt és egy számot.");
 
             var account = new Account()
             {
+                ID = Guid.NewGuid(),
                 Email = email,
                 Password = password
             };
@@ -50,7 +53,9 @@ namespace UnitTestExample.Controllers
 
         public bool ValidatePassword(string password)
         {
-            return true;
+            return Regex.IsMatch(
+                password,
+                @"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$");
         }
     }
 }
